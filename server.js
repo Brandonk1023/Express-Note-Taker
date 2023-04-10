1814c70ecb4fba6eda2622834060d42007f0d074
Brandon Kilgore <brandonk1023@gmail.com>
2023-04-10 12:41:02 -0400

    added post method code for server.js

--- server.js
+++ server.js
@@ -7,23 +7,24 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/notes', (req, res) => {
-res.sendFile(path.join(__dirname, "public/notes.html"))
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 })
 
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, "public/index.html"))
+    res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
 app.get('/api/notes', (req, res) => {
-res.sendFile(path.join(__dirname, "db/db.json"))
+    res.sendFile(path.join(__dirname, "db/db.json"))
 })
 
 app.post('/api/notes', (req, res) => {
-
+    let newNote = req.body
+    let notes = JSON.parse(fs.readFile('db/db.json', 'utf-8'))
 })
 
-app.listen(PORT, () => console.log("Listening on port" + PORT))
+app.listen(PORT, () => console.log("Listening on port " + PORT))
