5eb42e8c99fb45cb2588af16b9fd186d0778f5ca
Brandon Kilgore <brandonk1023@gmail.com>
2023-04-10 11:08:59 -0400

    added routing code for GET request

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.get('/notes', (req, res) => {
-
+res.sendFile(path.join(__dirname, "public/notes.html"))
 })
 
 app.get('*', (req, res) => {
-
+res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
 app.get('/api/notes', (req, res) => {
-
+res.sendFile(path.join(__dirname, "db/db.json"))
 })
 
 app.post('/api/notes', (req, res) => {
