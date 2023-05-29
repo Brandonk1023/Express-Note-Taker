aba520f67a895a18265702e2119944d3e89162c8
Brandon Kilgore <brandonk1023@gmail.com>
2023-05-29 14:06:10 -0400

    added more routing coding to the server.js file.  Added some comments

--- server.js
+++ server.js
@@ -2,44 +2,57 @@ const fs = require('fs')
 const express = require('express')
 const {v4 : uuidv4} = require('uuid')
 const path = require('path')
-
+const noteList = require('./db/db.json')
 const app = express()
-
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3001
 
+app.use()
+
+//middleware for public
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+
+//route to notes.html
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
+//route gets notes from db.json
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (data) => {
-        let notesList = JSON.parse(data) 
-    })
+    fs.readFile('./db/db.json', 'utf8', (err) => {if (err) console.log(err)})
     res.json(noteList)
 })
 
+//route post new note
 app.post('/api/notes', (req, res) => {
-    fs.readFile('db/db.json', 'utf-8')
-    .then(data => {
-      let savedNotes = JSON.parse(data)
-      let userNote = req.body
-      userNote.id = uuidv4()
-
-      savedNotes.push(userNote)
+    noteList.push(req.body)
 
     fs.writeFile("./db/db.json", JSON.stringify(savedNotes))
 
     res.json(savedNotes)
 
 })
-})
+
+// delete note
+app.delete("api/notes/:id", function (req, res) {
+    if (req.params.id) {
+        const noteId = req.params.id;
+        for (let i = 0; i < noteList.length; i ++) {
+            if (noteList[i].id === noteId) {
+                noteList.splice(i, 1)
+            }
+        }
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {console.log(err)})
+    res.json(`Deleted note ${req.params.id}`)
+}
 
 app.listen(PORT, () => console.log("Listening on port " + PORT))
